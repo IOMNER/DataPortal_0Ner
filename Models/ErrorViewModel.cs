21a6d046d64933bef8b020f0bc87a8d13783a675
michelo243 <misamuna@gmail.com>
2022-02-15 15:28:59 +0100

    Project Initialization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataPortal_0Ner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
